8b8b149f2778d53055410be1dc0fc851ab067162
medipallyAbinay <medipallyabinay@gmail.com>
2022-12-16 22:18:07 +0530

    partially Tested

--- src/Componenets/Algorithms.js
+++ src/Componenets/Algorithms.js
@@ -1,19 +1,17 @@
 import React, { useEffect, useState } from "react";
 
 function Algorithms(props) {
- // console.log(props.props)
-  
-  const [FCFSOP,setFCFSOP]=useState([])
-  const [PRIORITYOP,setPRIORITYOP]=useState([])
-  const [SJFOP,setSJFOP]=useState([])  
+
   const [FA1,setFA1]=useState([])
   const [FA2,setFA2]=useState([])
   const [FA3,setFA3]=useState([])
+  const [tbt1,setTBT1]=useState(0)
+  const [tbt2,setTBT2]=useState(0)
+  const [tbt3,setTBT3]=useState(0)
      
   const divideToAlgorithm=()=>{
     let tbt=0;
     let tarray=props.props.sort((a,b)=>{return b.Priority-a.Priority}).map((element,index)=>element)
-   // console.log(tarray)
     tarray.map((element,index)=>{
         tbt+=element.BT
         return ;
@@ -32,7 +30,7 @@ function Algorithms(props) {
     let tzidx=100000;
     let FcfsArray=tarray.map((element,index)=>{
         if(index<=zidx) return;
-        if(i<=tbt){
+        if(i<tbt){
             i+=element.BT;
             tzidx=index;
             return element;
@@ -42,15 +40,12 @@ function Algorithms(props) {
         if(index<=tzidx) return;
         else return element;
     })
-    console.log("prio",PriorityArray)
-    console.log("fcfs",FcfsArray)
-    console.log("sjf",SJFArray)
-    setPRIORITYOP(PRIORITY(PriorityArray));
-    setFCFSOP(FCFS(FcfsArray));
-    setSJFOP(SJF(SJFArray));
+    PRIORITY(PriorityArray);
+    FCFS(FcfsArray);
+    SJF(SJFArray);
   }
   const FCFS = (objArray) => {
-    let ans = 0;
+    let ans = 0,tbt=0,i=0;
     let tempAns = objArray
       .sort((a, b) => {
         return a.AT - b.AT;
@@ -59,18 +54,23 @@ function Algorithms(props) {
         return element;
       });
     let FinalAns = tempAns.map((element, index) => {
-      if(element===undefined) return 
+      if(element===undefined) return
+      if(element.AT>ans) ans+=element.AT-ans 
       ans += element.BT;
-      element.CT = ans;
+      element.CT=ans
       element.TAT = element.CT - element.AT;
       element.WT = element.TAT - element.BT;
+      tbt+=element.WT;
+      i+=1
       return element;
     });
     setFA1(FinalAns);
+    if(i===0) i=1
+    setTBT1(tbt/i);
   };
 
   const PRIORITY = (objArray) => {
-    let ans = 0;
+    let ans = 0,tbt=0,i=0;
 
     let intermediateArray = objArray
       .sort((a, b) => {
@@ -79,20 +79,24 @@ function Algorithms(props) {
       .map((element, index) => {
         return element;
       });
-      //console.log(intermediateArray)
     let finalAns = intermediateArray.map((element, index) => {
       if(element===undefined) return 
+      if(element.AT>ans) ans+=element.AT-ans 
       ans += element.BT;
       element.CT = ans;
       element.TAT = element.CT - element.AT;
       element.WT = element.TAT - element.BT;
+      tbt+=element.WT;
+      i+=1
       return element;
     });
    setFA2(finalAns);
+   setTBT2(tbt);
+   if(i!==0) setTBT2(tbt/i);
   };
 
   const SJF = (objArray) => {
-    let ans = 0;
+    let ans = 0,tbt=0,i=0;
     let tempArray = objArray
       .sort((a, b) => {
         return a.AT - b.AT;
@@ -109,14 +113,18 @@ function Algorithms(props) {
       });
     let finalAns = intermediateArray.map((element, index) => {
       if(element===undefined) return 
+      if(element.AT>ans) ans+=element.AT-ans 
       ans += element.BT;
       element.CT = ans;
       element.TAT = element.CT - element.AT;
       element.WT = element.TAT - element.BT;
+      tbt+=element.WT;
+      i+=1
       return element;
     });
-   
     setFA3(finalAns)
+    if(i===0) i=1;
+    setTBT3(tbt/i)
   };
 
   useEffect(() => {
@@ -126,28 +134,34 @@ function Algorithms(props) {
   return <div>
    <div className="container">
     <div className="flex justify-content-evenly">
+    <div className="container p-5">
         <table className="table">
           <thead className="thead-dark">
-            <th>PID</th>
-            <th>Arrival Time</th>
-            <th>Burst Time</th>
-            <th>Completion Time</th>
-            <th>Waiting Time</th>
-            <th>Priority</th>
+          <tr>
+            <th scope="col" >PID</th>
+            <th scope="col" >Arrival Time</th>
+            <th scope="col" >Burst Time</th>
+            <th scope="col" >Completion Time</th>
+            <th scope="col" >Turn Around Time</th>
+            <th scope="col" >Waiting Time</th>
+            <th scope="col" >Priority</th>
+            </tr>
           </thead>
+          
           <tbody>
            
             {
 
-              FA1.length!=0 && FA1.map((element,index)=>{
+              FA1.length!==0 && FA1.map((element,index)=>{
               
                 if(element!==undefined) {
                   return(
                 <tr>
-                  <td>{element.PID}</td>
+                  <td scope="row">{element.PID}</td>
                   <td>{element.AT}</td>
                   <td>{element.BT}</td>
                   <td>{element.CT}</td>
+                  <td>{element.TAT}</td>
                   <td>{element.WT}</td>
                   <td>{element.Priority}</td>
                 </tr>
@@ -160,20 +174,26 @@ function Algorithms(props) {
             }
           </tbody>
         </table>
+        <strong className="">The Average Waiting Time is <span>{tbt1}</span></strong>
+        </div>
+        <div className="container p-5">
         <table className="table">
           <thead className="thead-dark">
-            <th>PID</th>
-            <th>Arrival Time</th>
-            <th>Burst Time</th>
-            <th>Completion Time</th>
-            <th>Waiting Time</th>
-            <th>Priority</th>
+          <tr>
+            <th scope="col" >PID</th>
+            <th scope="col" >Arrival Time</th>
+            <th scope="col" >Burst Time</th>
+            <th scope="col" >Completion Time</th>
+            <th scope="col" >Turn Around Time</th>
+            <th scope="col" >Waiting Time</th>
+            <th scope="col" >Priority</th>
+            </tr>
           </thead>
           <tbody>
            
             {
 
-              FA2.length!=0 && FA2.map((element,index)=>{
+              FA2.length!==0 && FA2.map((element,index)=>{
                 if(element!==undefined) {
                   return(
                 <tr>
@@ -181,6 +201,7 @@ function Algorithms(props) {
                   <td>{element.AT}</td>
                   <td>{element.BT}</td>
                   <td>{element.CT}</td>
+                  <td>{element.TAT}</td>
                   <td>{element.WT}</td>
                   <td>{element.Priority}</td>
                 </tr>
@@ -190,12 +211,16 @@ function Algorithms(props) {
             }
           </tbody>
         </table>
+        <strong className="">The Average Waiting Time is <span>{tbt2}</span></strong>
+        </div>
+        <div className="container p-5">
         <table className="table">
           <thead className="thead-dark">
             <th>PID</th>
             <th>Arrival Time</th>
             <th>Burst Time</th>
             <th>Completion Time</th>
+            <th>Turn Around Time</th>
             <th>Waiting Time</th>
             <th>Priority</th>
           </thead>
@@ -203,7 +228,7 @@ function Algorithms(props) {
            
             {
 
-              FA3.length!=0 && FA3.map((element,index)=>{
+              FA3.length!==0 && FA3.map((element,index)=>{
                 if(element!==undefined) {
                   return(
                 <tr>
@@ -211,6 +236,7 @@ function Algorithms(props) {
                   <td>{element.AT}</td>
                   <td>{element.BT}</td>
                   <td>{element.CT}</td>
+                  <td>{element.TAT}</td>
                   <td>{element.WT}</td>
                   <td>{element.Priority}</td>
                 </tr>
@@ -221,6 +247,8 @@ function Algorithms(props) {
             }
           </tbody>
         </table>
+        <strong className="">The Average Waiting Time is <span>{tbt3}</span></strong>
+    </div>
     </div>
    </div>
   </div>;
--- src/Componenets/InputReader.js
+++ src/Componenets/InputReader.js
@@ -1,107 +1,181 @@
-import React from 'react'
-import { useState } from 'react'
-import Algorithms from './Algorithms'
+import React from "react";
+import { useState } from "react";
+import Algorithms from "./Algorithms";
 
 function InputReader() {
-const [pid,setPid]=useState('')
-const [at,setAt]=useState('')
-const [bt,setBt]=useState('')
-const [priority, setPriority] = useState('')
-const [tempNum, setTempNum] = useState('')
-const [numberofP, setnumberofP] = useState()
-const [allProcesses,setAllprocess]=useState([])
-const [state,setState]=useState(true);
-const [totalP,setTotalP]=useState([])
-const handleSubmit=()=>{
-    setnumberofP( numberofP-1);
-    let obj={
-        PID: +(pid),
-      AT: +(at),
-      BT: +(bt),
+  const [pid, setPid] = useState("");
+  const [at, setAt] = useState("");
+  const [bt, setBt] = useState("");
+  const [priority, setPriority] = useState("");
+  const [tempNum, setTempNum] = useState("");
+  const [numberofP, setnumberofP] = useState();
+  const [allProcesses, setAllprocess] = useState([]);
+  const [state, setState] = useState(true);
+  const [state1, setState1] = useState(true);
+  const [totalP, setTotalP] = useState([]);
+  const handleSubmit = () => {
+    setnumberofP(numberofP - 1);
+    let obj = {
+      PID: +pid,
+      AT: +at,
+      BT: +bt,
       CT: 0,
       TAT: 0,
       WT: 0,
-      Priority: priority!=''?+(priority):0,
+      Priority: priority !== "" ? +priority : 0,
+    };
+    let temp = allProcesses;
+    temp.push(obj);
+    setAllprocess(temp);
+    if (numberofP === 1) {
+      setState(false);
     }
-    let temp=allProcesses
-    temp.push(obj)
-    setAllprocess(temp)
-    if(numberofP==1) {  setState(false)
-        console.log(state)
-    }
-    setPid('')
-    setAt('')
-    setBt('')
-    setPriority('')
-}
+    setPid("");
+    setAt("");
+    setBt("");
+    setPriority("");
+  };
 
-const getNumberOfProcesses=()=>{
-    setnumberofP(+(tempNum))
-    let temp=[]
-    for(let i=1;i<=tempNum;i++) {
-        temp=totalP
-        temp.push(i);
-        setTotalP(temp);
-    }  
-}
+  const getNumberOfProcesses = () => {
+    setnumberofP(+tempNum);
+    let temp = [];
+    for (let i = 1; i <= tempNum; i++) {
+      temp = totalP;
+      temp.push(i);
+      setTotalP(temp);
+    }
+  };
 
   return (
     <div>
-        <div className="container">
-        {state && <>
-        <div className="display-1 mb-5">Enter Process details</div>
-        <div className="form">
-            <label htmlFor="number" className="form-label">Number of processes</label>
-            <input type="text" name="number" id="number" className="form-control" onChange={(e)=>{
-                setTempNum(e.target.value)
-                }} />
-            <button className="btn btn-success rounded" onClick={getNumberOfProcesses} > submit </button>
-        </div>
-        {numberofP>0 && 
-        totalP.map((element,index)=>{
-            return(
-                <>
-                <div className="row">
-            <div className="col-sm-10">
-            <form  className="form"  >
-                <div className="row">
-                    <div className="col-sm-3">
-                        <label htmlFor="pid" className="form-label">Process Id</label>
-                        <input type="text" name="pid" id="pid" className="form-control" onChange={(e)=>setPid(e.target.value)} />
-                    </div>
-                    <div className="col-sm-3">
-                        <label htmlFor="at" className="form-label">Arrival Time</label>
-                        <input type="text" name="at" id="at" className="form-control" onChange={(e)=>{setAt(e.target.value)}}  />
-                    </div>
-                    <div className="col-sm-3">
-                        <label htmlFor="bt" className="form-label">Burst Time</label>
-                        <input type="text" name="bt" id="bt" className="form-control" onChange={(e)=>{setBt(e.target.value)}} />
-                    </div>
-                    <div className="col-sm-3">
-                        <label htmlFor="priority" className="form-label">priority</label>
-                        <input type="text" name="priority" id="priority" className="form-control" onChange={(e)=>{setPriority(e.target.value)}} />
-                    </div>
-                </div>
-            </form>
+      <div
+        className="container mt-5 shadow shadow-lg"
+        style={{ background: "#FFFFFF", minHeight: "800px" }}
+      >
+        {state && (
+          <>
+            <div className="display-1 mb-5">ENTER PROCESS DETAILS</div>
+            <div className="form row">
+              <div className="col-sm-2"></div>
+              <div className="col-sm-6">
+                <label htmlFor="number" className="form-label m-3">
+                  <span className="display-5"> Number of processes </span>
+                </label>
+                <input
+                  type="text"
+                  name="number"
+                  id="number"
+                  className="form-control shadow shadow-md m-1"
+                  onChange={(e) => {
+                    setTempNum(e.target.value);
+                  }}
+                />
+              </div>
+              <div className="col-sm-4 mt-5">
+                <button
+                  className="btn btn-success rounded mt-5"
+                  onClick={getNumberOfProcesses}
+                >
+                  {" "}
+                  submit{" "}
+                </button>
+              </div>
             </div>
-            <div className="col-sm-2">
-                <button className="btn btn-success rounded mt-4" onClick={handleSubmit} >Submit</button>
-            </div>
-        </div>
-                </>
-            )
-        })
-       
-        }
-        </> }
-        {
-            state===false && <div>
+            {numberofP > 0 &&
+              totalP.map((element, index) => {
+                return (
+                  <>
+                    <div className="row mt-5">
+                      <div className="col-sm-10">
+                        <form className="form">
+                          <div className="row">
+                            <div className="col-sm-3">
+                              <label htmlFor="pid" className="form-label">
+                                Process Id
+                              </label>
+                              <input
+                                type="text"
+                                name="pid"
+                                id="pid"
+                                className="form-control"
+                                onChange={(e) => setPid(e.target.value)}
+                              />
+                            </div>
+                            <div className="col-sm-3">
+                              <label htmlFor="at" className="form-label">
+                                Arrival Time
+                              </label>
+                              <input
+                                type="text"
+                                name="at"
+                                id="at"
+                                className="form-control"
+                                onChange={(e) => {
+                                  setAt(e.target.value);
+                                }}
+                              />
+                            </div>
+                            <div className="col-sm-3">
+                              <label htmlFor="bt" className="form-label">
+                                Burst Time
+                              </label>
+                              <input
+                                type="text"
+                                name="bt"
+                                id="bt"
+                                className="form-control"
+                                onChange={(e) => {
+                                  setBt(e.target.value);
+                                }}
+                              />
+                            </div>
+                            <div className="col-sm-3">
+                              <label htmlFor="priority" className="form-label">
+                                priority
+                              </label>
+                              <input
+                                type="text"
+                                name="priority"
+                                id="priority"
+                                className="form-control"
+                                onChange={(e) => {
+                                  setPriority(e.target.value);
+                                }}
+                              />
+                            </div>
+                          </div>
+                        </form>
+                      </div>
+                      <div className="col-sm-2">
+                        <button
+                          className="btn rounded mt-4 shadow shadow-md text-white"
+                          style={{ background: "#a9a9a9" }}
+                          onClick={handleSubmit}
+                        >
+                          Add to queue
+                        </button>
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
+          </>
+        )}
+        {state === false && (
+          <div>
+          
+            {state1===true ? (
+              <div>
                 <Algorithms props={allProcesses} />
-            </div>
-        }
-        </div>
+              </div>
+            ):(
+                <button className="btn btn-success" onClick={setState1(false)} >State Execution</button>
+            )}
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default InputReader
+export default InputReader;
